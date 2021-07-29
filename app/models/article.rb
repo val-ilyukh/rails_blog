f0ebea6ca32e782fce817da887602409ee25015b
Valeriy Ilyukh <v.ilyukh@yandex.ru>
2021-07-29 15:59:20 +0300

    Added picture for an article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 	has_many :comments
-
+	mount_uploader :picture, PictureUploader
 	validates :title, presence: {message: 'Поле не должно быть пустым'}
 	validates :body, presence: {message: 'Поле не должно быть пустым'}, length: {minimum: 10, message: 'Статья должна иметь длину не менее 10 символов'}
 end
